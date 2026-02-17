f8fcaa0e7e3a8597e05fc4cd3f89cbaa4c2da8b8
ibrahim farhan shlaka <ibrahimasslan4@gmail.com>
2026-02-17 18:54:49 +0300

    gtag function fix

--- src/utils/gtag.js
+++ src/utils/gtag.js
@@ -1,14 +1,14 @@
-export function reportCallConversion(url) {
-  if (!window.gtag) return;
-
-  const callback = () => {
-    if (url) window.location = url;
+function gtag_report_conversion(url) {
+  var callback = function () {
+    if (typeof(url) != 'undefined') {
+      window.location = url;
+    }
   };
-
-  window.gtag("event", "conversion", {
-    send_to: "AW-17938224109/uTUXCIXUrfobEO2nzulC",
-    value: 1.0,
-    currency: "TRY",
-    event_callback: callback,
+  gtag('event', 'conversion', {
+      'send_to': 'AW-17938224109/uTUXCIXUrfobEO2nzulC',
+      'value': 1.0,
+      'currency': 'TRY',
+      'event_callback': callback
   });
+  return false;
 }
